b9bdb5566cf03806f45671486d3f1d88eef4f824
camille-s <camille.seaberry@gmail.com>
2020-02-28 10:12:50 -0500

    Better text contrast

--- src/components/Stage.jsx
+++ src/components/Stage.jsx
@@ -12,7 +12,7 @@ const Stage = ({ meta, children, location, ...restProps }) =>  (
         <Card.Subtitle>{ meta && compileHeader(location)({ location, ...meta, ...restProps }) }</Card.Subtitle>
         { children }
       </Card.Body>
-      <Card.Footer className='text-muted'>
+      <Card.Footer>
         <p>Source: 2019 National Assessment for Educational Progress overseen by the U.S. Department of Education</p>
         <p>Camille Seaberry, DataHaven</p>
       </Card.Footer>
